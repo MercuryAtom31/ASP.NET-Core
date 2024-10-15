ff7f3d49174a0eca9da1814a07b74aa52b990061
MercuryAtom31 <83618354+MercuryAtom31@users.noreply.github.com>
2024-10-14 23:29:21 -0400

    Add files via upload

---
+++ MovieReviewPlatform/Controllers/MovieReviewController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieReviewPlatform.Models;
+
+namespace MovieReviewPlatform.Controllers
+{
+    public class MovieReviewController : Controller
+    {
+        private static List<Movie> Reviews = new List<Movie>();
+
+        [HttpGet]
+        public IActionResult AddReview()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddReview(Movie model)
+        {
+            if (ModelState.IsValid)
+            {
+                Reviews.Add(model);
+
+                return RedirectToAction("Success");
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+
+        public IActionResult AllReviews()
+        {
+            return View(Reviews);
+        }
+    }
+}
---
+++ MovieReviewPlatform/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieReviewPlatform.Models
+{
+	public class Movie
+    {
+        [Required(ErrorMessage = "Title is required.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Director is required.")]
+        public string Director { get; set; }
+
+        [Required(ErrorMessage = "Release year is required.")]
+        [Range(1900, 2024, ErrorMessage = "Please enter a year between 1900 and the current year.")]
+        public int ReleaseYear { get; set; }
+
+        [Required(ErrorMessage = "Rating is required.")]
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5.")]
+        public int Rating { get; set; }
+
+        public string Review { get; set; }
+
+    }
+}
